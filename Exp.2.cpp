0a4ae20ab26c1ffad0969504fcd6f86f0f158650
SohamMangale <sohammangale60@gmail.com>
2024-08-10 17:20:33 +0530

    Create Exp.2.cpp

---
+++ Exp.2.cpp
@@ -0,0 +1,17 @@
+//Soham
+//23070123084
+//entc B1
+//experiment 2
+#include<iostream>
+using namespace std;
+int main() {
+    cout<<"size of char:"<<"sizeof(char)"<< "byte(s)"<<endl;
+    cout<<"size of int:"<<"sizeof(int)"<< "byte(s)"<<endl;
+    cout<<"size of short int:"<<"sizeof(short int)"<<"byte(s)"<<endl;
+    cout<<"size of long int:"<<"size of (long int)"<<"byte(s)"<<endl;
+    cout<<"size of float:"<<"size of(float)"<<"byte(s)"<<endl;
+    cout<<"size of double:"<<"size of(double)"<<"byte(s)"<<endl;
+    cout<<"size of long double:"<<"size of(long double)"<<endl;
+    cout<<"size of bool:"<<"size of(bool)"<<endl;
+    return 0;
+}
