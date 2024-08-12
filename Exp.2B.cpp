a6639f429441a2f8c89884a10fa75e74865380fa
SohamMangale <sohammangale60@gmail.com>
2024-08-12 21:09:15 +0530

    Create Exp.2B.cpp

---
+++ Exp.2B.cpp
@@ -0,0 +1,21 @@
+//Soham
+//23070123084
+//entc b1
+//experiment 2B
+#include<iostream>
+using namespace std;
+int main() {
+    int a;
+    cout<<"Enter any integer:";
+    cin>>a;
+    cout<<"\ninteger="<<a<<" and "<<sizeof(a)<< "bytes";
+    float b;
+    cout<<"Enter a number:";
+    cin>>b;
+    cout<<"\nfloat="<<b<<" and "<<sizeof(b)<< "bytes";
+    short int c;
+    cout<<"Enter an integer:";
+    cin>>c;
+    cout<<"\nshort integer"<<c<<" and "<<sizeof(c)<< "bytes";
+    return 0;
+}
